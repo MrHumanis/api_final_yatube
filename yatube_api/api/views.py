09f8ea3df617d5256901c4fa6ceea172c6461d5e
MrHumanis <160314122+MrHumanis@users.noreply.github.com>
2025-04-24 18:38:56 +0700

    Add files via upload

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,64 @@
-# TODO:  Напишите свой вариант
+from posts.models import Follow
+from .serializers import FollowSerializer
+from posts.models import Group, Comment, Post
+from .serializers import GroupSerializer, PostSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
+from .pagination import OptionalPagination
+from rest_framework import viewsets, permissions, filters
+from django.shortcuts import get_object_or_404
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['following__username']
+    pagination_class = None
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    pagination_class=None
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly
+    ]
+    pagination_class = OptionalPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsAuthorOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    pagination_class = None
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        new_queryset = post.comments.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        if serializer.is_valid():
+            serializer.save(author=self.request.user, post=post)
+        return super().perform_create(serializer)
--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Group, Post, Follow, Comment
+
+admin.site.register(Group)
+admin.site.register(Post)
+admin.site.register(Follow)
+admin.site.register(Comment)
