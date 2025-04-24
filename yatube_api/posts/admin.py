30c303993414984cbe11470ad5182d13e1a299f1
MrHumanis <160314122+MrHumanis@users.noreply.github.com>
2025-04-24 20:05:36 +0700

    Delete yatube_api directory

--- yatube_api/posts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Group, Post, Follow, Comment
-
-admin.site.register(Group)
-admin.site.register(Post)
-admin.site.register(Follow)
-admin.site.register(Comment)
